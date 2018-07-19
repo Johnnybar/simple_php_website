6d0a4ebae593965e098ad18c865ce8567578af1f
Jonathan Bareket <jonathan.bareket@e002166-imac27.ber.s-f.com>
2018-07-19 13:06:05 +0200

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+<div id="footer" class="cf">
+
+  <div class="column three">
+    <strong>Phone</strong>
+    808.555.1231
+  </div><!-- column -->
+  <div class="column three">
+    <strong>Location</strong>
+    19 Broker str. <br>
+    Chicago, Illinois
+  </div><!-- column -->
+  <div class="column three last">
+    <strong>Hours</strong>
+    <em>Tuesday - Thursday</em><br>
+    1:00pm - 9:00pm <br><br>
+
+    <em>Friday - Saturday</em><br>
+    4:00pm - 11:00pm <br><br>
+
+    <em>Sunday - Monday</em><br>
+    Closed <br><br>
+
+  </div><!-- column -->
+
+</div><!-- footer -->
+
+<small>&copy;<?php echo date('Y') ?> <?php echo $companyName; ?></small>
+
+</div><!-- content -->
+
+</div><!-- wrapper -->
+
+<div class="copyright-info">
+  <<?php include('../assets/includes/copyright.php') ?>
+</div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<?php
+$companyName = 'Franklin';
+include('includes/arrays.php')
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+  <link rel="stylesheet" href="/assets/styles.css">
+</head>
+
+<body id="final-example">
+  <div class="wrapper">
+
+    <div id="banner">
+      <a href="/" title = "Return to Home">
+        <img src="img/banner.png" alt="fine-dining">
+      </a>
+
+    </div><!-- banner -->
+    <div id="nav">
+      <?php include('includes/nav.php'); ?>
+    </div><!-- nav -->
+
+    <div class="content">
