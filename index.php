548d7d652b45686fee6ca698123c7944005520c3
Johnnybar <johnnybareket@gmail.com>
2018-07-23 12:58:19 +0000

    switched to main

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('includes/header.php');
    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat.
+      laboris nisi ut aliquip ex ea commodo.
       Duis aute irure dolor in reprehenderit in voluptate velit esse
      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
      cupidatat non proident, sunt in culpa qui officia deserunt mollit
